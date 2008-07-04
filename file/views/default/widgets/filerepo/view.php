ea85bf250fab91cc752ca2327711af014cbcc0a4
Dave Tosh <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-07-04 16:50:45 +0000

    small url bug fix in the file widget.
    git-svn-id: http://code.elgg.org/plugins/plugins@4184 36083f99-b078-4883-b0ff-0f9b5a30f544

--- file/views/default/widgets/filerepo/view.php
+++ file/views/default/widgets/filerepo/view.php
@@ -69,7 +69,7 @@ $('a.show_file_desc').click(function () {
         	
         	
         //get a link to the users files
-        $users_file_url = $vars['url'] . "mod/pg/file/" . get_user($f->owner_guid)->username;
+        $users_file_url = $vars['url'] . "pg/file/" . get_user($f->owner_guid)->username;
         	
         echo "<p><a href=\"{$users_file_url}\">" . elgg_echo('file:more') . "</a></p>";
         echo "</div>";
