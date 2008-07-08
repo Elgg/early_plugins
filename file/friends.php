1d116d90c7af37815257e53c7a65782b2a19eef4
Dave Tosh <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-07-08 17:24:28 +0000

    more file header tweaks
    git-svn-id: http://code.elgg.org/plugins/plugins@4225 36083f99-b078-4883-b0ff-0f9b5a30f544

--- file/friends.php
+++ file/friends.php
@@ -11,22 +11,20 @@
 	require_once(dirname(dirname(dirname(__FILE__))) . "/engine/start.php");
 	
 	set_context('search');
-	$body = elgg_view_title($title = elgg_echo('file:yours:friends'));
-	
+	$title = elgg_view_title($title = elgg_echo('file:yours:friends'));
+	$body = list_user_friends_objects(page_owner(),'file');
 	set_context('file');
 	if ($friends = get_user_friends($user_guid, $subtype, 999999, 0)) {
 		$friendguids = array();
 		foreach($friends as $friend) {
 			$friendguids[] = $friend->getGUID();
 		}
-		$body .= get_filetype_cloud($friendguids);
+		$filter_options = get_filetype_cloud($friendguids);
 	} else {
-		$body .= "";
+		$filter_options = "";
 	}
 
-	$body .= list_user_friends_objects(page_owner(),'file');
-
-	$body = elgg_view_layout('one_column',$body);
+	$body = elgg_view_layout('one_column',$title . $filter_options . $body);
 	
 	// Finally draw the page
 	page_draw(sprintf(elgg_echo("file:friends"),$_SESSION['user']->name), $body);
