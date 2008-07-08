05cce2b1b5e49eac881c031405a78e2636ccba41
Dave Tosh <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-07-08 17:19:03 +0000

    more file header tweaks
    git-svn-id: http://code.elgg.org/plugins/plugins@4224 36083f99-b078-4883-b0ff-0f9b5a30f544

--- file/index.php
+++ file/index.php
@@ -14,12 +14,19 @@
 	//require_once(dirname(dirname(dirname(__FILE__))) . "/engine/start.php");
 	require_once(dirname(dirname(dirname(__FILE__))) . "/engine/start.php");
 
+	//set the title
+		if(page_owner() == $_SESSION['user']){
+			$title = elgg_view_title($title = elgg_echo('file:yours'));
+		}else{
+			$title = elgg_view_title($title = elgg_echo('files'));
+		}
+
 	// Get objects
 		set_context('search');
 		$objects = list_entities("object","file",page_owner(),10,false);
 		set_context('file');
 		$filelist = get_filetype_cloud(page_owner());
-		$body = elgg_view_layout('one_column', $filelist . $objects);
+		$body = elgg_view_layout('one_column', $title . $filelist . $objects);
 	
 	// Finally draw the page
 		page_draw(sprintf(elgg_echo("file:user"),page_owner_entity()->name), $body);
