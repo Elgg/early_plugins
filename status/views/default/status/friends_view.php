23ba70f1ecc7e7f92c7c19aef86b9bbceddc1120
Dave Tosh <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-07-18 16:18:15 +0000

    friend status link fixed
    git-svn-id: http://code.elgg.org/plugins/plugins@4286 36083f99-b078-4883-b0ff-0f9b5a30f544

--- status/views/default/status/friends_view.php
+++ status/views/default/status/friends_view.php
@@ -24,7 +24,7 @@
 	    <!-- display the message -->
         <p>
     			<?php
-    				echo elgg_echo("status") . ": " . parse_urls(elgg_view("output/longtext",array("value" => $vars['entity']->description)));
+    				echo elgg_echo("status") . ": " . parse_urls(elgg_view("output/text",array("value" => $vars['entity']->description)));
     			?>
         </p>
     		
