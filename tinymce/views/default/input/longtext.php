4651ec1b5d5250f5bd8a6e09e92cd0f4a7a53f94
Dave Tosh <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-07-18 13:14:25 +0000

    tweaks to the tinymce plugin
    git-svn-id: http://code.elgg.org/plugins/plugins@4280 36083f99-b078-4883-b0ff-0f9b5a30f544

--- tinymce/views/default/input/longtext.php
+++ tinymce/views/default/input/longtext.php
@@ -24,7 +24,7 @@
    tinyMCE.init({
 	mode : "textareas",
 	theme : "advanced",
-	theme_advanced_buttons1 : "bold,italic,underline,separator,strikethrough,justifyleft,justifycenter,justifyright, justifyfull,bullist,numlist,undo,redo,link,unlink",
+	theme_advanced_buttons1 : "bold,italic,underline,separator,strikethrough,justifyleft,justifycenter,justifyright, justifyfull,bullist,numlist,undo,redo,link,unlink,image,blockquote,code",
 	theme_advanced_buttons2 : "",
 	theme_advanced_buttons3 : "",
 	theme_advanced_toolbar_location : "top",
