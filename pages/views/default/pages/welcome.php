5669864d29780872a7f866e78a040e959b31b7b3
Dave Tosh <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-08-03 18:12:07 +0000

    a placeholder pages welcome
    git-svn-id: http://code.elgg.org/plugins/plugins@4373 36083f99-b078-4883-b0ff-0f9b5a30f544

---
+++ pages/views/default/pages/welcome.php
@@ -0,0 +1,16 @@
+<?php
+
+    /**
+	 * Elgg Pages welcome message
+	 * 
+	 * @package ElggPages
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Curverider
+	 * @copyright Curverider Ltd 2008
+	 * @link http://elgg.com/
+	 */
+
+?>
+
+<p>Welcome to my wiki page set up. This will b e replaced by a user defined welcome message. Something like so:</p>
+<p>Welcome to my Wiki pages! Here I will post content and resources for all who are interested. You can navigate through my wiki pages by clicking on the main categories below, have fun.</p>
