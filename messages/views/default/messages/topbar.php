933011a53f117df69019d1449b009742e3c0e96d
Pete Harris <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-08-05 20:30:25 +0000

    toolbar fixed for messages
    git-svn-id: http://code.elgg.org/plugins/plugins@4405 36083f99-b078-4883-b0ff-0f9b5a30f544

--- messages/views/default/messages/topbar.php
+++ messages/views/default/messages/topbar.php
@@ -25,13 +25,13 @@
 
 ?>
 
-	<a href="<?php echo $vars['url']; ?>pg/messages/<?php echo $_SESSION['user']->username; ?>" class="pagelinks privatemessages" ></a>
+	<a href="<?php echo $vars['url']; ?>pg/messages/<?php echo $_SESSION['user']->username; ?>" class="privatemessages" >&nbsp;</a>
 	
 <?php
     }else{
 ?>
 
-    <a href="<?php echo $vars['url']; ?>pg/messages/<?php echo $_SESSION['user']->username; ?>" class="pagelinks privatemessages_new" >[<?php echo $num; ?>]</a>
+    <a href="<?php echo $vars['url']; ?>pg/messages/<?php echo $_SESSION['user']->username; ?>" class="privatemessages_new" >[<?php echo $num; ?>]</a>
 	
 <?php
     }
