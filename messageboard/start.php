d7f3d2aabd4199cdfe06f45a1db72fc580a48765
Dave Tosh <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-08-09 14:37:21 +0000

    messageboard widget is now only available for a users profile.
    git-svn-id: http://code.elgg.org/plugins/plugins@4446 36083f99-b078-4883-b0ff-0f9b5a30f544

--- messageboard/start.php
+++ messageboard/start.php
@@ -39,7 +39,7 @@
 			register_page_handler('messageboard','messageboard_page_handler');
         
 	    // add a messageboard widget
-            add_widget_type('messageboard',"". elgg_echo("messageboard:board") . "","" . elgg_echo("messageboard:desc") . ".");
+            add_widget_type('messageboard',"". elgg_echo("messageboard:board") . "","" . elgg_echo("messageboard:desc") . ".", "profile");
             
         // extend the group members view
             extend_view('groups/left_column','messageboard/group_messageboard');
