1855cdb25cad89ec65878af2beb42ca2dc4b4004
Ben Werdmuller <ben@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-08-22 10:45:50 +0000

    Changed TinyMCE allowed styles
    git-svn-id: http://code.elgg.org/plugins/plugins@4537 36083f99-b078-4883-b0ff-0f9b5a30f544

--- tinymce/views/default/input/longtext.php
+++ tinymce/views/default/input/longtext.php
@@ -31,7 +31,7 @@
 	theme_advanced_toolbar_align : "left",
 	theme_advanced_statusbar_location : "bottom",
 	theme_advanced_resizing : true,
-	extended_valid_elements : "a[name|href|target|title|onclick],img[class|src|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name],hr[class|width|size|noshade],font[face|size|color|style],span[class|align|style]"
+	extended_valid_elements : "a[name|href|target|title|onclick],img[class|src|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name|style],hr[class|width|size|noshade],font[face|size|color|style],span[class|align|style]"
 });
 </script>
 <!-- show the textarea -->
