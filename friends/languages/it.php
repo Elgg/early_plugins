6e8791a49ba1480255f545dd4c52f0d1314c9566
Marcus Povey <marcus@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-09-10 18:21:06 +0000

    Italian language patch.
    git-svn-id: http://code.elgg.org/plugins/plugins@4555 36083f99-b078-4883-b0ff-0f9b5a30f544

---
+++ friends/languages/it.php
@@ -0,0 +1,31 @@
+<?php
+
+     /**
+	 * Elgg Plugin language pack
+	 * 
+	 * @package Elgg Friends
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Curverider Ltd
+	 * @copyright Curverider Ltd 2008
+	 * @link http://elgg.com/
+	 * ****************************************
+     * @Italian Language Pack
+     * @Plugin: Friends
+     * @version: beta 00000
+     * @translation by Lord55  
+     * @link http://www.nobilityofequals.com
+     ****************************************/
+
+	$italian = array(
+	
+		/**
+		 * Friends widget   ###Amici widget###
+		 */
+			'friends:widget:description' => "Visualizza alcuni dei tuoi amici.",
+	        
+		
+	);
+					
+	add_translation("it",$italian);
+
+?>
