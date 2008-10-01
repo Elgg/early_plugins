0f676c2457a0ac54105a9710c970f55394d6ee8d
Ben Werdmuller <ben@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-10-01 18:26:34 +0000

    Removed GUIDs from page titles.
    git-svn-id: http://code.elgg.org/plugins/plugins@4577 36083f99-b078-4883-b0ff-0f9b5a30f544

--- pages/views/default/pages/sidebar/sidebarthis.php
+++ pages/views/default/pages/sidebar/sidebarthis.php
@@ -26,7 +26,7 @@
 	function pages_draw_child($childentity, $path) {
 		
 				$child = "\n\t\t{\n";
-				$child .= "\t\t\t\"text\": \"<a href=\\\"{$childentity->getURL()}\\\">{$childentity->guid} {$childentity->title}</a>\"\n";
+				$child .= "\t\t\t\"text\": \"<a href=\\\"{$childentity->getURL()}\\\">{$childentity->title}</a>\"\n";
 				
 				$extras = "";
 				$haschild = get_entities_from_metadata('parent_guid',$childentity->guid,'','',0,9999);
