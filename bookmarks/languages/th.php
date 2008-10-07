b8ae56b9b39660e7eb7ba8cdc0454437b27a2020
Marcus Povey <marcus@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-10-07 09:25:51 +0000

    Thai language pack
    git-svn-id: http://code.elgg.org/plugins/plugins@4595 36083f99-b078-4883-b0ff-0f9b5a30f544

---
+++ bookmarks/languages/th.php
@@ -0,0 +1,71 @@
+<?php
+
+	$thai = array(
+	
+		/**
+		 * Menu items and titles
+		 */ 
+	
+			'bookmarks' => "บุคส์มาค",
+			'bookmarks:add' => "บุคส์มาคบางสิ่ง",
+			'bookmarks:read' => "บุคส์มาค",
+			'bookmarks:friends' => "บุคส์มาคของเพื่อน",
+			'bookmarks:everyone' => "บุคส์มาคทั้งหมด",
+			'bookmarks:this' => "บุคส์มาคหน้านี้",
+			'bookmarks:bookmarklet' => "รายละเอียดบุคส์มาค",
+			'bookmarks:inbox' => "กล่องบุคส์มาค",
+			'bookmarks:more' => "บุคส์มาคอื่นๆ",
+			'bookmarks:shareditem' => "บุคส์มาค",
+			'bookmarks:with' => "แบ่งปันโดย",
+	
+			'bookmarks:address' => "เพิ่มไปยังบุคส์มาค",
+	
+			'bookmarks:delete:confirm' => "แน่ใจไหมที่จะลบ?",
+	
+			'bookmarks:shared' => "บุคส์มาค",
+			'bookmarks:visit' => "การเข้าชม",
+			'bookmarks:recent' => "บุคส์มาคล่าสุด",
+	
+			'bookmarks:river:created' => '%s บุคส์มาค',
+			'bookmarks:river:annotate' => '%s เปิดความคิดเห็น',
+			'bookmarks:river:item' => 'อันเดียว',
+	
+			'item:object:bookmarks' => 'บุคส์มาค',
+	
+	
+		/**
+		 * More text
+		 */
+		    
+		    'bookmarks:widget:description' => 
+		            "วิดเจ็ตออกแบบมาเพื่อแสดงบุคส์มาค",
+	
+			'bookmarks:bookmarklet:description' =>
+					"บุคส์มาคสามารแบ่งปันไปยังเพื่อนของคุณได้, ",
+
+	        'bookmarks:bookmarklet:descriptionie' =>
+					"หากคุณใช้ Internet Explorer,คุรต้องคลิ๊กขวาที่ไอคอนบุคส์มาค, เลือก 'add to favorites'",
+
+			'bookmarks:bookmarklet:description:conclusion' =>
+					"คุณสามารถบันทึกบุคส์มาคได้ทุกหน้า",
+	
+		/**
+		 * Status messages
+		 */
+	
+			'bookmarks:save:success' => "บุคส์มาคแล้ว",
+			'bookmarks:delete:success' => "บุคส์มาคลบแล้ว",
+	
+		/**
+		 * Error messages
+		 */
+	
+			'bookmarks:save:failed' => "ไม่สามารถบันทึกบุคส์มาคได้",
+			'bookmarks:delete:failed' => "ไม่สามารถลบบุคส์มาคได้",
+	
+	
+	);
+					
+	add_translation("th",$thai);
+
+?>
