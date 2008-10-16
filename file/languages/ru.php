9ab4b1197a2c1a958b55764a33bdbe6e6a33f712
Marcus Povey <marcus@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-10-16 09:06:11 +0000

    Russian language pack for file
    git-svn-id: http://code.elgg.org/plugins/plugins@4633 36083f99-b078-4883-b0ff-0f9b5a30f544

---
+++ file/languages/ru.php
@@ -0,0 +1,96 @@
+<?php
+  /**
+   * Elgg file plugin language pack
+   * 
+   * @package ElggFile
+   * @author Curverider Ltd
+   * @copyright Curverider Ltd 2008
+   * @link http://elgg.com/
+   */
+
+$russian = array(
+    
+                 /**
+                  * Menu items and titles
+                  */
+    
+                 'file' => 'Файлы',
+                 'files' => 'Файлы',
+                 'file:yours' => 'Ваши файлы',
+                 'file:yours:friends' => 'Файлы Ваших друзей',
+                 'file:user' => 'Файлы %s',
+                 'file:friends' => 'Файлы друзей пользователя %s',
+                 'file:all' => 'Файлы всего сайта',
+                 'file:more' => 'больше файлов',
+                 'file:list' => 'Просмотреть списком',
+                 'file:group' => 'Файлы группы',
+                 'file:gallery' => 'Просмотреть как галерею',
+                 'file:gallery_list' => 'Просмотреть как галерею или списком',
+                 'file:num_files' => 'Число файлов для отображения',
+    
+                 'file:upload' => 'Закачать файл',
+            
+                 'file:file' => 'Файл',
+                 'file:title' => 'Название',
+                 'file:desc' => 'Описание',
+                 'file:tags' => 'Теги',
+    
+                 'file:types' => 'Типы закачанных файлов',
+    
+                 'file:type:all' => 'Все файлы',
+                 'file:type:video' => 'Видео',
+                 'file:type:document' => 'Документы',
+                 'file:type:audio' => 'Аудио',
+                 'file:type:image' => 'Изображения',
+                 'file:type:general' => 'Остальные',
+    
+                 'file:user:type:video' => 'Видео пользователя %s',
+                 'file:user:type:document' => 'Документы пользователя %s',
+                 'file:user:type:audio' => 'Аудио пользователя %s',
+                 'file:user:type:image' => 'Изображения пользователя %s',
+                 'file:user:type:general' => 'Остальные файлы пользователя %s',
+    
+                 'file:friends:type:video' => 'Видео Ваших друзей',
+                 'file:friends:type:document' => 'Документы Ваших друзей',
+                 'file:friends:type:audio' => 'Аудио Ваших друзей',
+                 'file:friends:type:image' => 'Изображения Ваших друзей',
+                 'file:friends:type:general' => 'Остальные Ваших друзей',
+    
+                 'file:widget' => 'Ваши файлы',
+                 'file:widget:description' => "Показывает ваши последние файлы",
+    
+                 'file:download' => 'Скачать',
+    
+                 'file:delete:confirm' => 'Вы уверены что хотите удалить этот файл?',
+            
+                 'file:tagcloud' => 'Облако тэгов',
+    
+                 'file:display:number' => 'Число файлов для отображения',
+    
+                 'file:river:created' => '%s закачал',
+                 'file:river:item' => 'файл',
+                 'file:river:annotate' => '%s прокомментировал на ',
+
+                 'item:object:file' => 'Файлы',
+    
+                 /**
+                  * Status messages
+                  */
+    
+                 'file:saved' => 'Ваш файл успешно сохранен.',
+                 'file:deleted' => 'Ваш файл успешно удален.',
+    
+                 /**
+                  * Error messages
+                  */
+    
+                 'file:none' => 'В данный момент мы не можем найти никаких файлов.',
+                 'file:uploadfailed' => 'Простите, но мы не можем сохранить Ваш файл.',
+                 'file:downloadfailed' => 'Простите, но этот файл в данный момент не доступен.',
+                 'file:deletefailed' => 'Ваш файл в данный момент не может быть удален.',
+    
+                 );
+
+add_translation("ru",$russian);
+
+?>
