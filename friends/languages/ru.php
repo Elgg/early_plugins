18e6bdcb6c854e81864cc7cbdf09b3e2a4811cda
Marcus Povey <marcus@36083f99-b078-4883-b0ff-0f9b5a30f544>
2008-10-16 09:08:50 +0000

    Friends russian language pack
    git-svn-id: http://code.elgg.org/plugins/plugins@4636 36083f99-b078-4883-b0ff-0f9b5a30f544

---
+++ friends/languages/ru.php
@@ -0,0 +1,15 @@
+<?php
+
+	$russian = array(
+	
+		/**
+		 * Friends widget
+		 */
+			'friends:widget:description' => "Показывает некоторых ваших друзей.",
+	        
+		
+	);
+					
+	add_translation("ru",$russian);
+
+?>
