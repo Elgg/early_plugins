92ad0767e0d71d9fbd8ef5628a815d1b232ce099
Ben Werdmuller <ben@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-01-13 18:34:42 +0000

    The file edit screen now sets page owner appropriately. Fixes #679
    git-svn-id: http://code.elgg.org/plugins/plugins@4690 36083f99-b078-4883-b0ff-0f9b5a30f544

--- file/edit.php
+++ file/edit.php
@@ -15,8 +15,22 @@
 	// Render the file upload page
 	
 	$file = (int) get_input('file_guid');
-	$title = elgg_view_title($title = elgg_echo('file:edit'));
 	if ($file = get_entity($file)) {
+		
+		// Set the page owner
+		$page_owner = page_owner_entity();
+		if ($page_owner === false || is_null($page_owner)) {
+			$container_guid = $file->container_guid;
+			if (!empty($container_guid))
+				if ($page_owner = get_entity($container_guid)) {
+					set_page_owner($container_guid->guid);
+				}
+			if (empty($page_owner)) {
+				$page_owner = $_SESSION['user'];
+				set_page_owner($_SESSION['guid']);
+			}
+		}
+		$title = elgg_view_title($title = elgg_echo('file:edit'));
 		if ($file->canEdit()) { 
     		$area2 = elgg_view("file/upload",array('entity' => $file));
 			$body = elgg_view_layout('two_column_left_sidebar', '', $title . $area2);
