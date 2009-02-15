4dba61c73cd34424cb1fee7f62471129ef4ff733
Dave Tosh <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-02-15 20:18:42 +0000

    the walled garden now removes all content from the frontpage.
    git-svn-id: http://code.elgg.org/plugins/plugins@4743 36083f99-b078-4883-b0ff-0f9b5a30f544

--- walledgarden/start.php
+++ walledgarden/start.php
@@ -19,7 +19,22 @@
 		elgg_set_viewtype('default');
 		
 		if (current_page_url() != $CONFIG->url) 
-			extend_view('pageshells/pageshell', 'walledgarden/walledgarden'); 
+			extend_view('pageshells/pageshell', 'walledgarden/walledgarden');
+		
+		// Extend system CSS with our own styles
+				extend_view('css','walledgarden/css');
+				
+       // Replace the default index page
+			register_plugin_hook('index','system','custom_index');
+			
+
+	}
+	
+	function custom_index() {
+			
+			if (!@include_once(dirname(dirname(__FILE__))) . "/walledgarden/index.php") return false;
+			return true;
+			
 	}
 	
 	register_elgg_event_handler('init','system','walledgarden_init');
