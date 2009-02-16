14e6a7edae60267623ea49ba70f39fadbbac98fe
Dave Tosh <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-02-16 20:31:16 +0000

    new class added to expages
    git-svn-id: http://code.elgg.org/plugins/plugins@4753 36083f99-b078-4883-b0ff-0f9b5a30f544

--- externalpages/views/default/expages/menu.php
+++ externalpages/views/default/expages/menu.php
@@ -11,6 +11,9 @@
 	 * 
 	 */
 	 
+	 //type
+	 $type = $vars['type'];
+	 
 	 //set the url
 	 $url = $vars['url'] . "pg/expages/index.php?type=";
 
@@ -18,9 +21,9 @@
 
 <div id="friendsPickerNavigationTabs">
 <ul>
-	<li><a href="<?php echo $url; ?>front"><?php echo elgg_echo('expages:frontpage'); ?></a></li>
-	<li><a href="<?php echo $url; ?>about"><?php echo elgg_echo('expages:about'); ?></a></li>
-	<li><a href="<?php echo $url; ?>terms"><?php echo elgg_echo('expages:terms'); ?></a></li>
-	<li><a href="<?php echo $url; ?>privacy"><?php echo elgg_echo('expages:privacy'); ?></a></li>
+	<li <?php if($type == 'front') echo "class = 'selected'"; ?>><a href="<?php echo $url; ?>front"><?php echo elgg_echo('expages:frontpage'); ?></a></li>
+	<li <?php if($type == 'about') echo "class = 'selected'"; ?>><a href="<?php echo $url; ?>about"><?php echo elgg_echo('expages:about'); ?></a></li>
+	<li <?php if($type == 'terms') echo "class = 'selected'"; ?>><a href="<?php echo $url; ?>terms"><?php echo elgg_echo('expages:terms'); ?></a></li>
+	<li <?php if($type == 'privacy') echo "class = 'selected'"; ?>><a href="<?php echo $url; ?>privacy"><?php echo elgg_echo('expages:privacy'); ?></a></li>
 </ul>
 </div>
