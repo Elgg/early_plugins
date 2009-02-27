beea5dd655e788f70ce65e6b1dedf4cfce5a3a6f
Pete Harris <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-02-27 13:31:50 +0000

    default icon modified to use size variable
    git-svn-id: http://code.elgg.org/plugins/plugins@4858 36083f99-b078-4883-b0ff-0f9b5a30f544

--- file/views/default/file/icon/default.php
+++ file/views/default/file/icon/default.php
@@ -1,5 +1,10 @@
 <?php
 
-	echo "{$vars['url']}mod/file/graphics/icons/general.gif";
+	if ($vars['size'] == 'large') {
+		$ext = '_lrg';
+	} else {
+		$ext = '';
+	}
+	echo "{$CONFIG->wwwroot}mod/theme_new_elgg_default/graphics/file_icons/general{$ext}.gif";
 
 ?>
