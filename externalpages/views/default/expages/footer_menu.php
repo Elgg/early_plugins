fa22ac75e5cf822a1e4c3f57a8a9b3228c98f327
Pete Harris <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-03-02 18:55:41 +0000

    tiny addition - added a pipe to the external pages footer links
    git-svn-id: http://code.elgg.org/plugins/plugins@4884 36083f99-b078-4883-b0ff-0f9b5a30f544

--- externalpages/views/default/expages/footer_menu.php
+++ externalpages/views/default/expages/footer_menu.php
@@ -14,7 +14,7 @@
 	
 ?>
 
-<div class="footer_toolbar_links">
+<div class="footer_toolbar_links">| 
 <a href="<?php echo $vars['url']; ?>pg/expages/read/About/"><?php echo elgg_echo('expages:about'); ?></a> |
 <a href="<?php echo $vars['url']; ?>pg/expages/read/Terms/"><?php echo elgg_echo('expages:terms'); ?></a> |
 <a href="<?php echo $vars['url']; ?>pg/expages/read/Privacy/"><?php echo elgg_echo('expages:privacy'); ?></a> |
