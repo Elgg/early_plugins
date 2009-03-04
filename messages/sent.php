da05a88de5e5626cb8adb3251a34c9823b583022
Ben Werdmuller <ben@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-03-04 12:44:11 +0000

    The sent page now properly paginates
    git-svn-id: http://code.elgg.org/plugins/plugins@4893 36083f99-b078-4883-b0ff-0f9b5a30f544

--- messages/sent.php
+++ messages/sent.php
@@ -20,15 +20,21 @@
 		$page_owner = $_SESSION['user'];
 		set_page_owner($page_owner->guid);
 		
+	// Get offset
+		$offset = get_input('offset',0);
+	
+	// Set limit
+		$limit = 10;
+		
     // Display all the messages a user owns, these will make up the sentbox
-		$messages = get_entities_from_metadata('fromId',$_SESSION['user']->guid,'object','messages', $page_owner->guid, 999, 0); 
+		$messages = get_entities_from_metadata('fromId',$_SESSION['user']->guid,'object','messages', $page_owner->guid, $limit, $offset); 
 		//$page_owner->getObjects('messages');
 		
     // Set the page title
 	    $area2 = elgg_view_title(elgg_echo("messages:sentmessages"));
 		
 	// Set content
-		$area2 .= elgg_view("messages/view",array('entity' => $messages, 'page_view' => "sent"));
+		$area2 .= elgg_view("messages/view",array('entity' => $messages, 'page_view' => "sent", 'limit' => $limit, 'offset' => $offset));
 
 	// Format
 		$body = elgg_view_layout("two_column_left_sidebar", '', $area2);
