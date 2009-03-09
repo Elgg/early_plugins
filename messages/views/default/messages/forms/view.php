ad6c0778344ce6797253f96ff802ed4b5cdb66c8
Ben Werdmuller <ben@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-03-09 13:09:59 +0000

    Page redirect improvement.
    git-svn-id: http://code.elgg.org/plugins/plugins@4942 36083f99-b078-4883-b0ff-0f9b5a30f544

--- messages/views/default/messages/forms/view.php
+++ messages/views/default/messages/forms/view.php
@@ -3,6 +3,7 @@
 	$body = elgg_view("messages/view",$vars);
 	
 	$body .= '<div class="messages_buttonbank">';
+	$body .= '<input type="hidden" name="type" value="'.$vars['page_view'].'" />';
 	$body .= '<input type="submit" name="submit" value="'.elgg_echo('delete').'" /> ';
 	$body .= '<input type="submit" name="submit" value="'.elgg_echo('messages:markread').'" /> ';
 	$body .= '<input type="button" onclick="javascript:$(\'input[@type=checkbox]\').click();" value="'.elgg_echo('messages:toggle').'" />';
