f1690f2a7f8e4b860ddd5bf7042972e2288725d2
Ben Werdmuller <ben@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-04-07 16:21:35 +0000

    jQuery Form is no longer necessary in the embed plugin.
    git-svn-id: http://code.elgg.org/plugins/plugins@4962 36083f99-b078-4883-b0ff-0f9b5a30f544

--- embed/views/default/embed/metatags.php
+++ embed/views/default/embed/metatags.php
@@ -2,5 +2,4 @@
 		 jQuery(document).ready(function($) {
 		  $('a[rel*=facebox]').facebox()
 		});
-		</script>
-		<script type="text/javascript" src="<?php echo $vars['url']; ?>mod/embed/jquery.form.js"></script> 
+		</script> 
