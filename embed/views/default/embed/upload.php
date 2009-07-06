888e6b9c8ff9bb6f3166d057af5f62fa85081af9
Brett Profitt <brettp@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-07-06 21:58:44 +0000

    Changed variable names for new multi file upload.
    git-svn-id: http://code.elgg.org/plugins/plugins@5017 36083f99-b078-4883-b0ff-0f9b5a30f544

--- embed/views/default/embed/upload.php
+++ embed/views/default/embed/upload.php
@@ -1,7 +1,7 @@
 <h1 class="mediaModalTitle">Embed / Upload Media</h1>
 <?php
 
-	echo elgg_view('embed/tabs',array('tab' => 'upload'));
+	echo elgg_view('embed/tabs',array('tab' => 'upload', 'internalname'=>$vars['internalname']));
 
 	if (!elgg_view_exists('file/upload')) {
 		echo "<p>" . elgg_echo('embed:file:required') . "</p>";
@@ -9,61 +9,62 @@
 		$action = 'file/upload';
 		
 ?>
-	<form id="mediaUpload" action="<?php echo $vars['url']; ?>action/file/upload" method="post" enctype="multipart/form-data">
-		<p>
-			<label for="upload"><?php echo elgg_echo("file:file"); ?><br />
-		<?php
-
-				echo elgg_view("input/file",array('internalname' => 'upload', 'js' => 'id="upload"'));
-			
-		?>
-		</label></p>
-		<p>
-			<label><?php echo elgg_echo("title"); ?><br />
+	<form id="mediaUpload" action="<?php echo $vars['url']; ?>action/file/upload" method="POST" enctype="multipart/form-data">
+			<p>
+				<label for="upload"><?php echo elgg_echo("file:file"); ?><br />
 			<?php
-
-				echo elgg_view("input/text", array(
-									"internalname" => "title",
-									"value" => $title,
-													));
-			
+	
+					echo elgg_view("input/file",array('internalname' => 'upload_0', 'js' => 'id="upload"'));
+				
 			?>
-			</label>
-		</p>
-		<p>
-		<label for="filedescription"><?php echo elgg_echo("description"); ?><br />
-		<textarea class="input-textarea" name="description" id="filedescription"></textarea>
-		</label></p>
+			</label></p>
+			<p>
+				<label><?php echo elgg_echo("title"); ?><br />
+				<?php
+	
+					echo elgg_view("input/text", array(
+										"internalname" => "title_0",
+										"value" => $title,
+														));
+				
+				?>
+				</label>
+			</p>
+			<p>
+			<label for="filedescription"><?php echo elgg_echo("description"); ?><br />
+			<textarea class="input-textarea" name="description_0" id="filedescription"></textarea>
+			</label></p>
+			
+			<p>
+				<label><?php echo elgg_echo("tags"); ?><br />
+				<?php
+					echo elgg_view("input/tags", array(
+										"internalname" => "tags",
+										"value" => $tags,
+														));
 		
-		<p>
-			<label><?php echo elgg_echo("tags"); ?><br />
-			<?php
-				echo elgg_view("input/tags", array(
-									"internalname" => "tags",
-									"value" => $tags,
-													));
+				?>
+				</label>
+			</p>
+			<p>
+				<label>
+					<?php echo elgg_echo('access'); ?><br />
+					<?php echo elgg_view('input/access', array('internalname' => 'access_id','value' => $access_id)); ?>
+				</label>
+			</p>
+		
+			<p>
+				<?php
 	
-			?>
-			</label>
-		</p>
-		<p>
-			<label>
-				<?php echo elgg_echo('access'); ?><br />
-				<?php echo elgg_view('input/access', array('internalname' => 'access_id','value' => $access_id)); ?>
-			</label>
-		</p>
-	
-		<p>
-			<?php
-
-				if (isset($vars['container_guid']))
-					echo "<input type=\"hidden\" name=\"container_guid\" value=\"{$vars['container_guid']}\" />";
-				if (isset($vars['entity']))
-					echo "<input type=\"hidden\" name=\"file_guid\" value=\"{$vars['entity']->getGUID()}\" />";
-			
-			?>
-			<input type="submit" value="<?php echo elgg_echo("save"); ?>" />
-		</p>
+					if (isset($vars['container_guid']))
+						echo "<input type=\"hidden\" name=\"container_guid\" value=\"{$vars['container_guid']}\" />";
+					if (isset($vars['entity']))
+						echo "<input type=\"hidden\" name=\"file_guid\" value=\"{$vars['entity']->getGUID()}\" />";
+				
+					echo "<input type=\"hidden\" name=\"number_of_files\" value=\"1\" />";
+				?>
+				<input type="submit" value="<?php echo elgg_echo("save"); ?>" />
+			</p>
 	</form>
 	<script type="text/javascript"> 
         // wait for the DOM to be loaded 
