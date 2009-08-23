0749f6cc673bdb8ff0fa1df476bf2cb8ab8e8db1
Dave Tosh <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-08-23 11:32:23 +0000

    display a small portion of a new blog post in the river
    git-svn-id: http://code.elgg.org/plugins/plugins@5043 36083f99-b078-4883-b0ff-0f9b5a30f544

--- blog/views/default/river/object/blog/create.php
+++ blog/views/default/river/object/blog/create.php
@@ -7,7 +7,13 @@
 	$url = "<a href=\"{$performed_by->getURL()}\">{$performed_by->name}</a>";
 	$string = sprintf(elgg_echo("blog:river:created"),$url) . " ";
 	$string .= elgg_echo("blog:river:create") . " <a href=\"" . $object->getURL() . "\">" . $object->title . "</a>";
-	
+	$string .= "<div class=\"river_content_display\">";
+	if(strlen($object->description) > 200) {
+        	$string .= substr($object->description, 0, strpos($object->description, ' ', 200)) . "...";
+    }else{
+	    $string .= $object->description;
+    }
+	$string .= "</div>";
 ?>
 
 <?php echo $string; ?>
