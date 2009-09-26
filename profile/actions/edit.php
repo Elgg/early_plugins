9d684a11fd0e4529808971120e8799e87e0a0a4e
Dave Tosh <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-09-26 09:06:24 +0000

    add users default access level to edit profile appearing in the river
    git-svn-id: http://code.elgg.org/plugins/plugins@5098 36083f99-b078-4883-b0ff-0f9b5a30f544

--- profile/actions/edit.php
+++ profile/actions/edit.php
@@ -68,7 +68,7 @@
 			trigger_elgg_event('profileupdate',$user->type,$user);
 			
 			//add to river
-			add_to_river('river/user/default/profileupdate','update',$_SESSION['user']->guid,$_SESSION['user']->guid);
+			add_to_river('river/user/default/profileupdate','update',$_SESSION['user']->guid,$_SESSION['user']->guid,get_default_access($_SESSION['user']));
 			
 			system_message(elgg_echo("profile:saved"));
 			
