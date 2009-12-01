7f2ccc4f27be68b8b648b7f9687c47a5899c562c
Cash Costello <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2009-12-01 13:08:12 +0000

    added check in action to prevent normal user from cropping someone else's profile icon
    git-svn-id: http://code.elgg.org/plugins/plugins@5118 36083f99-b078-4883-b0ff-0f9b5a30f544

--- profile/actions/cropicon.php
+++ profile/actions/cropicon.php
@@ -13,63 +13,68 @@
 	gatekeeper();
 	action_gatekeeper();
 
-		$x1 = (int) get_input('x_1',0);
-		$y1 = (int) get_input('y_1',0);
-		$x2 = (int) get_input('x_2',0);
-		$y2 = (int) get_input('y_2',0);
+	$x1 = (int) get_input('x_1',0);
+	$y1 = (int) get_input('y_1',0);
+	$x2 = (int) get_input('x_2',0);
+	$y2 = (int) get_input('y_2',0);
 		
-		$user = page_owner_entity();
+	$user = page_owner_entity();
+	
+	if (!$user || !$user->canEdit()) {
+		register_error(elgg_echo("profile:icon:noaccess"));
+		forward();
+	}
 		
-		$filehandler = new ElggFile();
-		$filehandler->owner_guid = $user->getGUID();
-		$filehandler->setFilename("profile/" . $user->username . "master" . ".jpg");
-		$filename = $filehandler->getFilenameOnFilestore();
-		
-		$topbar = get_resized_image_from_existing_file($filename,16,16, true, $x1, $y1, $x2, $y2);
-		$tiny = get_resized_image_from_existing_file($filename,25,25, true, $x1, $y1, $x2, $y2);
-		$small = get_resized_image_from_existing_file($filename,40,40, true, $x1, $y1, $x2, $y2);
-		$medium = get_resized_image_from_existing_file($filename,100,100, true, $x1, $y1, $x2, $y2);
-		
-		if ($small !== false
-					&& $medium !== false
-					&& $tiny !== false) {
-				
-					$filehandler = new ElggFile();
-					$filehandler->owner_guid = $user->getGUID();
-					$filehandler->setFilename("profile/" .  $user->username . "medium.jpg");
-					$filehandler->open("write");
-					$filehandler->write($medium);
-					$filehandler->close();
-					$filehandler->setFilename("profile/" .  $user->username . "small.jpg");
-					$filehandler->open("write");
-					$filehandler->write($small);
-					$filehandler->close();
-					$filehandler->setFilename("profile/" .  $user->username . "tiny.jpg");
-					$filehandler->open("write");
-					$filehandler->write($tiny);
-					$filehandler->close();
-					$filehandler->setFilename("profile/" .  $user->username . "topbar.jpg");
-					$filehandler->open("write");
-					$filehandler->write($topbar);
-					$filehandler->close();
-					
-					$user->x1 = $x1;
-					$user->x2 = $x2;
-					$user->y1 = $y1;
-					$user->y2 = $y2;
-					
-					 $user->icontime = time();
-					
-					system_message(elgg_echo("profile:icon:uploaded"));
-				
-				} else {
-					system_message(elgg_echo("profile:icon:notfound"));					
-				}
+	$filehandler = new ElggFile();
+	$filehandler->owner_guid = $user->getGUID();
+	$filehandler->setFilename("profile/" . $user->username . "master" . ".jpg");
+	$filename = $filehandler->getFilenameOnFilestore();
+	
+	$topbar = get_resized_image_from_existing_file($filename,16,16, true, $x1, $y1, $x2, $y2);
+	$tiny = get_resized_image_from_existing_file($filename,25,25, true, $x1, $y1, $x2, $y2);
+	$small = get_resized_image_from_existing_file($filename,40,40, true, $x1, $y1, $x2, $y2);
+	$medium = get_resized_image_from_existing_file($filename,100,100, true, $x1, $y1, $x2, $y2);
+	
+	if ($small !== false
+				&& $medium !== false
+				&& $tiny !== false) {
 			
-	    //forward the user back to the upload page to crop
-	    
-	    $url = $vars['url'] . "pg/profile/{$user->username}/editicon/";
+				$filehandler = new ElggFile();
+				$filehandler->owner_guid = $user->getGUID();
+				$filehandler->setFilename("profile/" .  $user->username . "medium.jpg");
+				$filehandler->open("write");
+				$filehandler->write($medium);
+				$filehandler->close();
+				$filehandler->setFilename("profile/" .  $user->username . "small.jpg");
+				$filehandler->open("write");
+				$filehandler->write($small);
+				$filehandler->close();
+				$filehandler->setFilename("profile/" .  $user->username . "tiny.jpg");
+				$filehandler->open("write");
+				$filehandler->write($tiny);
+				$filehandler->close();
+				$filehandler->setFilename("profile/" .  $user->username . "topbar.jpg");
+				$filehandler->open("write");
+				$filehandler->write($topbar);
+				$filehandler->close();
+				
+				$user->x1 = $x1;
+				$user->x2 = $x2;
+				$user->y1 = $y1;
+				$user->y2 = $y2;
+				
+				 $user->icontime = time();
+				
+				system_message(elgg_echo("profile:icon:uploaded"));
 			
-		if (isloggedin()) forward($url);
+			} else {
+				register_error(elgg_echo("profile:icon:notfound"));
+			}
+		
+    //forward the user back to the upload page to crop
+    
+    $url = $vars['url'] . "pg/profile/{$user->username}/editicon/";
+		
+	if (isloggedin()) forward($url);
 
 ?>
