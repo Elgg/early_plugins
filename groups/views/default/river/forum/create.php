ff643aebc7a9f699648f55b8e4acd6c0778fdcd9
Dave Tosh <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-02-05 11:52:57 +0000

    a fix for ticket #323
    git-svn-id: http://code.elgg.org/plugins/plugins@5193 36083f99-b078-4883-b0ff-0f9b5a30f544

--- groups/views/default/river/forum/create.php
+++ groups/views/default/river/forum/create.php
@@ -5,20 +5,21 @@
 	//$url = $object->getURL();
 	$forumtopic = $object->guid;
 	$group_guid = $object->container_guid;
-	$comment = $object->getAnnotations("group_topic_post", 1, 0, "desc"); 
-	foreach($comment as $c){
-		$contents = $c->value;
-	}
+	//grab the annotation, if one exists
+	if($vars['item']->annotation_id != 0)
+		$comment = get_annotation($vars['item']->annotation_id)->value; 
 	$contents = strip_tags($contents);//this is so we don't get large images etc in the activity river
 	$url = $vars['url'] . "mod/groups/topicposts.php?topic=" . $forumtopic . "&group_guid=" . $group_guid;
 	$url_user = "<a href=\"{$performed_by->getURL()}\">{$performed_by->name}</a>";
 	$string = sprintf(elgg_echo("groupforum:river:posted"),$url_user) . " ";
 	$string .= elgg_echo("groupforum:river:annotate:create") . " | <a href=\"" . $url . "\">" . $object->title . "</a>";
 	$string .= "<div class=\"river_content_display\">";
-	if(strlen($contents) > 200) {
-        	$string .= substr($contents, 0, strpos($contents, ' ', 200)) . "...";
-    }else{
-	    $string .= $contents;
+	if($comment){
+		$contents = strip_tags($comment);//this is so we don't get large images etc in the activity river
+		if(strlen($contents) > 200)
+	        	$string .= substr($contents, 0, strpos($contents, ' ', 200)) . "...";
+	    else
+		    $string .= $contents;
     }
 	$string .= "</div>";
 ?>
